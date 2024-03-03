2069c1b483533bc8cfe92f21733ab1195ec59c43
sashok1987nik <sashok1987nk@ukr.net>
2024-03-03 16:10:50 +0100

    up

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,1 +1,32 @@
-console.log("Form");
+document.addEventListener('DOMContentLoaded', () => {
+    const form = document.querySelector('.feedback-form');
+    
+    form.addEventListener('input', (event) => {
+      const formData = {
+        email: form.querySelector('input[name="email"]').value,
+        message: form.querySelector('textarea[name="message"]').value
+      };
+  
+      localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+    });
+    
+    const savedFormData = JSON.parse(localStorage.getItem('feedback-form-state'));
+    if (savedFormData) {
+      form.querySelector('input[name="email"]').value = savedFormData.email;
+      form.querySelector('textarea[name="message"]').value = savedFormData.message;
+    }
+    
+    form.addEventListener('submit', (event) => {
+      event.preventDefault();
+      
+      const formData = JSON.parse(localStorage.getItem('feedback-form-state'));
+      
+      console.log({
+        email: formData.email,
+        message: formData.message
+      });
+      
+      localStorage.removeItem('feedback-form-state');
+      form.reset();
+    });
+  });
